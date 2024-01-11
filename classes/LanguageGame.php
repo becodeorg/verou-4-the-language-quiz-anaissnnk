281ae6a1d86ee60ba5e1e5f8822b73fbc38ee469
Anaïs <aekyon.08@gmail.com>
2024-01-11 12:28:13 +0100

    Handle right and wrong answer

--- classes/LanguageGame.php
+++ classes/LanguageGame.php
@@ -25,14 +25,32 @@ class LanguageGame
 
     public function run(): void
     {
-        // TODO: check for option A or B
-
-        // Option A: user visits site first time (or wants a new word)
-        // TODO: select a random word for the user to translate
-
         // Option B: user has just submitted an answer
         // TODO: verify the answer (use the verify function in the word class) - you'll need to get the used word from the array first
-        // TODO: generate a message for the user that can be shown
+        if($_SERVER["REQUEST_METHOD"] === "POST") {
+            $answer = htmlspecialchars($_POST["answer"]);
+            $selectedWord = htmlspecialchars($_POST["word"]);
+
+            //Define english translation
+            $correctEnglishTranslation = null;
+            foreach (Data::words() as $japaneseTranslation => $englishTranslation) {
+                if ($japaneseTranslation === $selectedWord) {
+                    $correctEnglishTranslation = $englishTranslation;
+                    break;
+                }
+            }
+
+            if ($correctEnglishTranslation !== null) {
+                // Compare the player's answer with the correct English translation
+                if ($answer === $correctEnglishTranslation) {
+                    echo "You got it right!";
+                } else {
+                    echo "Wrong answer!";
+                }
+            } else {
+                echo "Invalid selection"; // Handle the case when the selected word is not found
+            }
+        }
 
     }
 }
